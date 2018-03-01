282d7d687343304d2a577cec35eef00a066467b6
Murilo <murilomgm89@gmail.com>
2018-03-01 04:25:29 -0300

    s

--- Coupons/Promotion.Coupon.Entity/Handle/EmailHandle.cs
+++ Coupons/Promotion.Coupon.Entity/Handle/EmailHandle.cs
@@ -5,28 +5,32 @@ namespace Promotion.Coupon.Entity.Handle
 {
     public class EmailHandle
     {
-        public static void SendEmail(string from, string to, string subject, string message)
+        public static void SendEmail(string from, string to, string subject, string message, string ambiente)
         {
-            SmtpClient mySmtpClient = new SmtpClient("smtp.gmail.com");
-            mySmtpClient.Port = 587;
-            mySmtpClient.EnableSsl = true;
-            mySmtpClient.UseDefaultCredentials = true;
-            
-            // set smtp-client with basicAuthentication
-            mySmtpClient.UseDefaultCredentials = false;
-            System.Net.NetworkCredential basicAuthenticationInfo = new
-            System.Net.NetworkCredential("gomes.vmlbrasil@gmail.com", "z3277009@");
-            mySmtpClient.Credentials = basicAuthenticationInfo;
 
-            // add from,to mailaddresses
+            SmtpClient mySmtpClient = new SmtpClient("mailhost.gwsweb.net");
+            mySmtpClient.Port = 25;
+            mySmtpClient.UseDefaultCredentials = false;
+            mySmtpClient.DeliveryMethod = SmtpDeliveryMethod.Network;
+            System.Net.NetworkCredential basicAuthenticationInfo = new
+            System.Net.NetworkCredential("noreply@relay.kccweb.net", "Pass475866");
+            mySmtpClient.Credentials = basicAuthenticationInfo;
+                        
+            //INTERNO
+            SmtpClient mySmtpClient2 = new SmtpClient("smtp.gmail.com");
+            mySmtpClient2.Port = 587;
+            mySmtpClient2.EnableSsl = true;
+            mySmtpClient2.UseDefaultCredentials = false;
+            mySmtpClient2.DeliveryMethod = SmtpDeliveryMethod.Network;
+            System.Net.NetworkCredential basicAuthenticationInfo2 = new
+            System.Net.NetworkCredential("gomes.vmlbrasil@gmail.com", "z3277009@");                
+            mySmtpClient2.Credentials = basicAuthenticationInfo2;
+                       
+           
             MailAddress fromT = new MailAddress(from, "Promoção Intimus Sport");
             MailAddress toT = new MailAddress(to, "");
             MailMessage myMail = new System.Net.Mail.MailMessage(fromT, toT);
-
-            //// add ReplyTo
-            //MailAddress replyto = new MailAddress("reply@example.com");
-            //myMail.ReplyToList.Add(replyTo);
-
+       
             // set subject and encoding
             myMail.Subject = subject;
             myMail.SubjectEncoding = System.Text.Encoding.UTF8;
@@ -36,8 +40,14 @@ namespace Promotion.Coupon.Entity.Handle
             myMail.BodyEncoding = System.Text.Encoding.UTF8;
             // text or html
             myMail.IsBodyHtml = true;
-
-            mySmtpClient.Send(myMail);
+            if (ambiente == "Interno")
+            {
+                mySmtpClient2.Send(myMail);
+            }
+            else
+            {
+                mySmtpClient.Send(myMail);
+            }
         }
     }
 }
